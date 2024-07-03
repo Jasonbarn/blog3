a1516afca8f17b27f1f4c55e86b16347b8e79d83
jason <barnajason27@gmail.com>
2024-07-03 10:21:29 -0400

    creation de route avec le controller comment creation  d'u'ne function sstore dans le comment controller

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Models\Comment;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    //
+    public function store(Request $request, Post $post)
+
+    {
+        $request->validate([
+
+            'content'=>'required',
+        ]);
+
+        $comment = new Comment([
+            'content' => $request->content,
+            'user_id' => auth()->id(),
+            ]);
+  
+            $post->comments()->save($comment);
+
+
+            return redirect()->route('posts.show', $post->id)->with('success', 'Commentaire ajouté avec succès.');
+           }
+           
+
+    }
+
